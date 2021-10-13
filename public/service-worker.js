3fb71ccb477a5df93f20ec0c8df640dbde3bc70a
Mumtaz Natiq, GHR-OSA-NEX-21 <Natiq.Mumtaz@swisscom.com>
2021-10-14 00:57:02 +0200

    feat: hope for the fix

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,1 @@
-self.addEventListener('fetch', (event) => {
-	event.respondWith(
-		caches.match(event.request).then((response) => {
-			return response || fetch(event.request)
-		})
-	)
-})
+self.addEventListener('fetch', function (event) {})
