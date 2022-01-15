ca710ea2bb7a61d1f180ede16d570646c8cc23ac
Mumtaz Natiq, GHR-OSA-NEX-21 <Natiq.Mumtaz@swisscom.com>
2022-01-15 18:16:21 +0100

    feat: new icons and splash screens

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,3 @@
-export const isDev = false // process.env.NODE_ENV === 'development'
+export const isDev = process.env.NODE_ENV === 'development'
 export const api = "https://api.weatherbit.io/v2.0"
 export const API_KEY = process.env.NEXT_PUBLIC_API_KEY
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -25,35 +25,163 @@ function MyApp({ Component, pageProps }: AppProps) {
 	if (showLoader) return <Loader />
 
 	return (
-		<SWRConfig
-			value={{
-				refreshInterval: 15 * 60_000,
-				revalidateIfStale: false,
-				revalidateOnFocus: false,
-				revalidateOnReconnect: false,
-			}}
-		>
+		<React.Fragment>
 			<Head>
 				<title>WeatherPi</title>
 				<link rel="icon" href="/logo.png" />
 				<link rel="manifest" href="manifest.json" />
 				<meta name="theme-color" content="#ffffff" />
 
-				<link rel="icon" type="image/png" href="/images/icon-36x36.png" sizes="36x36" />
-				<link rel="icon" type="image/png" href="/images/icon-96x96.png" sizes="96x96" />
-				<link rel="icon" type="image/png" href="/images/icon-192.png" sizes="192x192" />
-
-				<link rel="apple-touch-icon" href="/images/icon-192.png" sizes="192x192" />
-				<meta name="msapplication-config" content="/iconx/browserconfig.xml" />
+				<link rel="apple-touch-icon" href="apple-icon-180.png" />
+				<meta name="apple-mobile-web-app-capable" content="yes" />
+				<link
+					rel="apple-touch-startup-image"
+					href="apple-splash-2048-2732.jpg"
+					media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+				/>
+				<link
+					rel="apple-touch-startup-image"
+					href="apple-splash-2732-2048.jpg"
+					media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+				/>
+				<link
+					rel="apple-touch-startup-image"
+					href="apple-splash-1668-2388.jpg"
+					media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+				/>
+				<link
+					rel="apple-touch-startup-image"
+					href="apple-splash-2388-1668.jpg"
+					media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+				/>
+				<link
+					rel="apple-touch-startup-image"
+					href="apple-splash-1536-2048.jpg"
+					media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+				/>
+				<link
+					rel="apple-touch-startup-image"
+					href="apple-splash-2048-1536.jpg"
+					media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+				/>
+				<link
+					rel="apple-touch-startup-image"
+					href="apple-splash-1668-2224.jpg"
+					media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+				/>
+				<link
+					rel="apple-touch-startup-image"
+					href="apple-splash-2224-1668.jpg"
+					media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+				/>
+				<link
+					rel="apple-touch-startup-image"
+					href="apple-splash-1620-2160.jpg"
+					media="(device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+				/>
+				<link
+					rel="apple-touch-startup-image"
+					href="apple-splash-2160-1620.jpg"
+					media="(device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+				/>
+				<link
+					rel="apple-touch-startup-image"
+					href="apple-splash-1284-2778.jpg"
+					media="(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+				/>
+				<link
+					rel="apple-touch-startup-image"
+					href="apple-splash-2778-1284.jpg"
+					media="(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
+				/>
+				<link
+					rel="apple-touch-startup-image"
+					href="apple-splash-1170-2532.jpg"
+					media="(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+				/>
+				<link
+					rel="apple-touch-startup-image"
+					href="apple-splash-2532-1170.jpg"
+					media="(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
+				/>
+				<link
+					rel="apple-touch-startup-image"
+					href="apple-splash-1125-2436.jpg"
+					media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+				/>
+				<link
+					rel="apple-touch-startup-image"
+					href="apple-splash-2436-1125.jpg"
+					media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
+				/>
+				<link
+					rel="apple-touch-startup-image"
+					href="apple-splash-1242-2688.jpg"
+					media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+				/>
+				<link
+					rel="apple-touch-startup-image"
+					href="apple-splash-2688-1242.jpg"
+					media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
+				/>
+				<link
+					rel="apple-touch-startup-image"
+					href="apple-splash-828-1792.jpg"
+					media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+				/>
+				<link
+					rel="apple-touch-startup-image"
+					href="apple-splash-1792-828.jpg"
+					media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+				/>
+				<link
+					rel="apple-touch-startup-image"
+					href="apple-splash-1242-2208.jpg"
+					media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+				/>
+				<link
+					rel="apple-touch-startup-image"
+					href="apple-splash-2208-1242.jpg"
+					media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
+				/>
+				<link
+					rel="apple-touch-startup-image"
+					href="apple-splash-750-1334.jpg"
+					media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+				/>
+				<link
+					rel="apple-touch-startup-image"
+					href="apple-splash-1334-750.jpg"
+					media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+				/>
+				<link
+					rel="apple-touch-startup-image"
+					href="apple-splash-640-1136.jpg"
+					media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+				/>
+				<link
+					rel="apple-touch-startup-image"
+					href="apple-splash-1136-640.jpg"
+					media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+				/>
 
 				<meta
 					name="viewport"
 					content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
 				/>
 			</Head>
-			<Component {...pageProps} />
-			<InstallPrompt />
-		</SWRConfig>
+			<SWRConfig
+				value={{
+					refreshInterval: 15 * 60_000,
+					revalidateIfStale: false,
+					revalidateOnFocus: false,
+					revalidateOnReconnect: false,
+				}}
+			>
+				<Component {...pageProps} />
+				<InstallPrompt />
+			</SWRConfig>
+		</React.Fragment>
 	)
 }
 
